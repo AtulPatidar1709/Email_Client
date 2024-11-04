3d8fe0ab0552a6abdef94e35772f4d55c1ec0434
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2024-11-04 14:09:44 +0530

    Improve Performance of the code

--- src/components/Emaillist.js
+++ src/components/Emaillist.js
@@ -128,16 +128,10 @@ const EmailList = () => {
     return true;
   });
 
-  if (loading)
-    return (
-      <div className="flex items-center justify-center">
-        <Loader />
-      </div>
-    );
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <Suspense fallback={<Loader />}>
+    <>
       <header className="flex flex-wrap items-center justify-start mb-4 pt-4 space-x-1">
         <h4 className="font-semibold">Filter By:</h4>
         {["all", "read", "unread", "favorites"].map((filterType) => (
@@ -153,101 +147,103 @@ const EmailList = () => {
           </button>
         ))}
       </header>
-      <main className="flex flex-col md:flex-row h-screen w-full">
-        <aside
-          className={`${
-            selectedEmail?.isRead ? "hidden md:block" : "block w-full"
-          } pr-3 gap-4 overflow-hidden transition-all duration-300 border-r border-gray-200`}
-        >
-          {filteredEmails.length === 0 ? (
-            <div className="flex items-center justify-center h-full">
-              <p>No emails found.</p>
-            </div>
-          ) : (
-            <div className="flex flex-col gap-4">
-              {filteredEmails.map((email) => (
-                <div
-                  key={email.emailId}
-                  className={`p-4 flex gap-4 rounded-lg cursor-pointer shadow-md transition-all duration-200 ${
-                    email.isRead ? "bg-gray-100" : "bg-white"
-                  } && ${
-                    selectedEmail?.emailId === email.emailId
-                      ? "border-2 border-red-500"
-                      : ""
-                  }`}
-                  onClick={() => handleEmailSelect(email)}
-                >
-                  {/* Sender Initial Circle */}
-                  <div className="bg-[#E54065] rounded-full h-12 w-12 flex items-center justify-center text-xl font-semibold text-white">
-                    {email.name.charAt(0).toUpperCase()}
-                  </div>
+      <Suspense fallback={<Loader />}>
+        <main className="flex flex-col md:flex-row h-screen w-full">
+          <aside
+            className={`${
+              selectedEmail?.isRead ? "hidden md:block" : "block w-full"
+            } pr-3 gap-4 overflow-hidden transition-all duration-300 border-r border-gray-200`}
+          >
+            {filteredEmails.length === 0 ? (
+              <div className="flex items-center justify-center h-full">
+                <p>No emails found.</p>
+              </div>
+            ) : (
+              <div className="flex flex-col gap-4">
+                {filteredEmails.map((email) => (
+                  <div
+                    key={email.emailId}
+                    className={`p-4 flex gap-4 rounded-lg cursor-pointer shadow-md transition-all duration-200 ${
+                      email.isRead ? "bg-gray-100" : "bg-white"
+                    } && ${
+                      selectedEmail?.emailId === email.emailId
+                        ? "border-2 border-red-500"
+                        : ""
+                    }`}
+                    onClick={() => handleEmailSelect(email)}
+                  >
+                    {/* Sender Initial Circle */}
+                    <div className="bg-[#E54065] rounded-full h-12 w-12 flex items-center justify-center text-xl font-semibold text-white">
+                      {email.name.charAt(0).toUpperCase()}
+                    </div>
 
-                  {/* Email Information */}
-                  <div className="flex-grow overflow-hidden flex flex-col gap-1">
-                    <p className="font-semibold">
-                      {email.name}{" "}
-                      <span className="text-base text-gray-500">
-                        {email.email}
-                      </span>
-                    </p>
-                    <p className="font-semibold truncate">
-                      Subject: {email.subject}
-                    </p>
-                    <p className="text-sm text-gray-500 truncate">
-                      {email.body}
-                    </p>
-                    <div className="flex justify-start gap-4 items-center text-sm">
-                      <p>{formatDate(email.createdAt)}</p>
-                      {email.isFavorite && (
-                        <span className="text-[#E54065] font-semibold">
-                          Favorite
+                    {/* Email Information */}
+                    <div className="flex-grow overflow-hidden flex flex-col gap-1">
+                      <p className="font-semibold">
+                        {email.name}{" "}
+                        <span className="text-base text-gray-500">
+                          {email.email}
                         </span>
-                      )}
+                      </p>
+                      <p className="font-semibold truncate">
+                        Subject: {email.subject}
+                      </p>
+                      <p className="text-sm text-gray-500 truncate">
+                        {email.body}
+                      </p>
+                      <div className="flex justify-start gap-4 items-center text-sm">
+                        <p>{formatDate(email.createdAt)}</p>
+                        {email.isFavorite && (
+                          <span className="text-[#E54065] font-semibold">
+                            Favorite
+                          </span>
+                        )}
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
-              {dataLength > 5 && (
-                <footer className="flex justify-center items-center rounded-full gap-4 mt-6">
-                  <button
-                    className={`px-4 py-2 rounded ${
-                      currentPage === 1
-                        ? "bg-red-300 text-gray-500 cursor-not-allowed"
-                        : "bg-red-200"
-                    }`}
-                    onClick={() => handlePageChange(currentPage - 1)}
-                    disabled={currentPage === 1}
-                  >
-                    Previous
-                  </button>
-                  <p>
-                    Page {currentPage} of {totalPages}
-                  </p>
-                  <button
-                    className={`px-4 py-2 rounded ${
-                      currentPage === totalPages
-                        ? "bg-red-300 text-gray-500 cursor-not-allowed"
-                        : "bg-red-200"
-                    }`}
-                    onClick={() => handlePageChange(currentPage + 1)}
-                    disabled={currentPage === totalPages}
-                  >
-                    Next
-                  </button>
-                </footer>
-              )}
+                ))}
+                {dataLength > 5 && (
+                  <footer className="flex justify-center items-center rounded-full gap-4 mt-6">
+                    <button
+                      className={`px-4 py-2 rounded ${
+                        currentPage === 1
+                          ? "bg-red-300 text-gray-500 cursor-not-allowed"
+                          : "bg-red-200"
+                      }`}
+                      onClick={() => handlePageChange(currentPage - 1)}
+                      disabled={currentPage === 1}
+                    >
+                      Previous
+                    </button>
+                    <p>
+                      Page {currentPage} of {totalPages}
+                    </p>
+                    <button
+                      className={`px-4 py-2 rounded ${
+                        currentPage === totalPages
+                          ? "bg-red-300 text-gray-500 cursor-not-allowed"
+                          : "bg-red-200"
+                      }`}
+                      onClick={() => handlePageChange(currentPage + 1)}
+                      disabled={currentPage === totalPages}
+                    >
+                      Next
+                    </button>
+                  </footer>
+                )}
+              </div>
+            )}
+          </aside>
+
+          {/* Email Body Section */}
+          {selectedEmail && (
+            <div className="block md:w-[80%] overflow-y-auto p-4 bg-white rounded-lg shadow-md">
+              {bodyloading ? <Loader /> : <EmailBody />}
             </div>
           )}
-        </aside>
-
-        {/* Email Body Section */}
-        {selectedEmail && (
-          <div className="block md:w-[80%] overflow-y-auto p-4 bg-white rounded-lg shadow-md">
-            {bodyloading ? <Loader /> : <EmailBody />}
-          </div>
-        )}
-      </main>
-    </Suspense>
+        </main>
+      </Suspense>
+    </>
   );
 };
 
